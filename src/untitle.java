b5c0b5fa94ae14965d873f4468a84d25c89dfac4
01686215808a <01686215808a@gmail.com>
2019-07-28 21:58:01 +0700

    bai tap ss1

---
+++ src/untitle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class untitle {
+    public static void main (String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int input;
+        int money;
+        System.out.println("enter the money");
+        input=scanner.nextInt();
+        money=input*23000;
+        System.out.println(money);
+    }
+}
